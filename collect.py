ea9c5ba496232c2d802d57faa90599b884f21589
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2026-03-21 20:31:48 +0000

    Filters are properly applied. Prefers TCP pings and traceroutes. Run everything in parallel in a pool. Saves the results.

--- collect.py
+++ collect.py
@@ -1,12 +1,14 @@
+from functools import partial
 import json
 import os
 from ripe.atlas.cousteau import AnchorRequest
 import pycountry
-from scapy.all import sr1
-from scapy.layers.inet import IP, UDP, ICMP
+from scapy.all import sr, sr1
+from scapy.layers.inet import IP, TCP, UDP, ICMP, traceroute
 import time
 import datetime
 from dataclasses import dataclass
+from multiprocessing import Pool
 
 def obtain_anchors() -> dict[str, list[AnchorRequest]]:
     results: dict[str, list[AnchorRequest]] = {}
@@ -19,7 +21,7 @@ def obtain_anchors() -> dict[str, list[AnchorRequest]]:
     else:
         for country in pycountry.countries:
             print(f"Querying anchors for {country.name}.")
-            anchors = AnchorRequest({"country": country.alpha_2, "limit": 10})
+            anchors = AnchorRequest(**{"country": country.alpha_2, "limit": 10})
 
             results[country.alpha_2] = list()
             for anchor in anchors:
@@ -36,45 +38,10 @@ def obtain_anchors() -> dict[str, list[AnchorRequest]]:
     print(f"{total} anchors obtained!")
     return results
 
-# https://www.geeksforgeeks.org/linux-unix/traceroute-implementation-on-python/
-def traceroute(destination: str, max_hops=30, timeout=2) -> int:
-    port = 33434
-    ttl = 1
-
-    while True:
-        # Creating the IP and UDP headers
-        ip_packet = IP(dst=destination, ttl=ttl)
-        udp_packet = UDP(dport=port)
-
-        # Combining the headers
-        packet = ip_packet / udp_packet
-
-        # Sending the packet and receive a reply
-        reply = sr1(packet, timeout=timeout, verbose=0)
-
-        if reply is None:
-            # No reply, print * for timeout
-            print(f"{ttl}\t")
-        elif reply.type == 3:
-            # Destination reached, print the details
-            print(f"{ttl}\t{reply.src}")
-            break
-        else:
-            # Printing the IP address of the intermediate hop
-            print(f"{ttl}\t{reply.src}")
-
-        ttl += 1
-
-        if ttl > max_hops:
-            break
-    
-    return ttl
-
-def ping(destination: str, timeout=2) -> float | None:
-    packet = IP(dst=destination, ttl=20)/ICMP()
-    reply = sr1(packet, timeout=timeout)
+def get_latency_tcp(destination: str) -> float | None:
+    packet=sr1(IP(dst=destination) / TCP(dport=80, flags="S"), timeout=10, verbose=False)
     start = time.time()
-    if not (reply is None):
+    if not (packet is None):
         return time.time() - start
     else:
         return 0
@@ -90,31 +57,30 @@ class Measure:
     guess: str = ""
     date_time: datetime.datetime = datetime.datetime.now()
 
-anchors = obtain_anchors()
-
-results = list()
-max_measurements = 3
-for country, anchors in anchors.items():
+def run_measurement_for_country(couple, max_measurements = 2, max_anchors = 10):
     count = 0
+    country, anchors = couple
+    results: list[Measure] = []
+    print(f"Starting {max_anchors} measurements for {country}.")
     for anchor in anchors:
-        both_measures = False
-
         destination = anchor["ip_v4"] # type: ignore
         if destination is None:
             destination = anchors["ip_v6"] # type: ignore
 
+        print(f"Taking measurements for {destination} in {country}.")
         latency = None
         hops = 0
 
-        for i in (1, max_measurements):
-            single_latency = ping(destination)
-            single_hops = traceroute(destination)
+        for i in (0, max_measurements):
+            single_latency = get_latency_tcp(destination)
+            # is a tcp syn traceroute
+            result, _ = traceroute(target=destination, verbose=False, dport=53) # is not supposed to answer on 53
 
             if single_latency is not None:
                 latency = 0
                 latency += single_latency
 
-            hops += single_hops
+            hops += len(result)
 
         if (latency is not None and hops > 0):
             count += 1
@@ -130,5 +96,30 @@ for country, anchors in anchors.items():
 
             results.append(measure)
         
-        if count > 10:
+        if count > max_anchors:
             break
+
+    print(f"End of measurements for {country}.")
+    return results
+
+
+if __name__ == '__main__':
+    anchors = obtain_anchors()
+
+    results = list()
+    max_measurements = 3
+
+    with Pool(4) as pool:
+        results = pool.map(run_measurement_for_country, anchors.items())
+    final_results = [
+        x
+        for xs in results
+        for x in xs
+    ]
+
+    json_str = json.dumps(results, indent=4)
+    with open("measurements.json", "w") as f:
+        f.write(json_str)
+
+    print("Saving measurements... Done!")
+
