bd4d21bee161a81b4116ca731fc6c4d64a39cd53
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2026-03-23 21:51:51 +0000

    Created a proper measure dataclass. Flatten results and process them.

--- collect.py
+++ collect.py
@@ -1,13 +1,14 @@
-from functools import partial
 import json
 import os
+from typing import Iterator, Optional, Self
+from pydantic import BaseModel, RootModel
 from ripe.atlas.cousteau import AnchorRequest
 import pycountry
-from scapy.all import sr, sr1
-from scapy.layers.inet import IP, TCP, UDP, ICMP, traceroute
+from scapy.all import sr1
+from scapy.layers.inet import IP, TCP, traceroute
 import time
 import datetime
-from dataclasses import dataclass
+from itertools import chain 
 from multiprocessing import Pool
 
 def obtain_anchors() -> dict[str, list[AnchorRequest]]:
@@ -46,21 +47,39 @@ def get_latency_tcp(destination: str) -> float | None:
     else:
         return 0
 
-@dataclass
-class Measure:
+class Measure(BaseModel):
     id: int
     ground_truth: str 
-    ip_v4: str
-    ip_v6: str
+    guess: Optional[str] = None
+    ip_v4: Optional[str] = None
+    ip_v6: Optional[str] = None
     latency: float
     hops: float
-    guess: str = ""
+    count: int
     date_time: datetime.datetime = datetime.datetime.now()
 
-def run_measurement_for_country(couple, max_measurements = 2, max_anchors = 10):
-    count = 0
+class Measures(RootModel):
+    root: list[Measure] = []
+
+    def __iter__(self) -> Iterator[Measure]: # type: ignore
+        return iter(self.root)
+
+    def __getitem__(self, item):
+        return self.root[item]
+    
+    def __add__(self, other: Self | list):
+        if isinstance(other, Measures):
+            return Measures(root=self.root + other.root)
+        elif isinstance(other, list):
+            return Measures(root=self.root + list(chain.from_iterable(other)))
+    
+    def append(self, measure: Measure):
+        self.root.append(measure)
+
+def run_measurement_for_country(couple, max_measures = 2, max_anchors = 10):
+    anchors_count = 0
     country, anchors = couple
-    results: list[Measure] = []
+    results = []
     print(f"Starting {max_anchors} measurements for {country}.")
     for anchor in anchors:
         destination = anchor["ip_v4"] # type: ignore
@@ -68,58 +87,54 @@ def run_measurement_for_country(couple, max_measurements = 2, max_anchors = 10):
             destination = anchors["ip_v6"] # type: ignore
 
         print(f"Taking measurements for {destination} in {country}.")
-        latency = None
+        latency = 0
         hops = 0
 
-        for i in (0, max_measurements):
+        measures_count = 0
+        failed_attempts = 0
+        while(not (measures_count > max_measures or failed_attempts > max_measures)):
             single_latency = get_latency_tcp(destination)
-            # is a tcp syn traceroute
             result, _ = traceroute(target=destination, verbose=False, dport=53) # is not supposed to answer on 53
 
             if single_latency is not None:
-                latency = 0
                 latency += single_latency
+                hops += len(result)
+                measures_count += 1
+            else:
+                failed_attempts += 1
 
-            hops += len(result)
-
-        if (latency is not None and hops > 0):
-            count += 1
+        if (measures_count > 0):
+            anchors_count += 1
 
             measure = Measure(
-                id=["id"],  # type: ignore
+                id=anchor["id"],  # type: ignore
                 ground_truth=country,
                 ip_v4=anchor["ip_v4"], # type: ignore
                 ip_v6=anchor["ip_v6"], # type: ignore
-                latency=latency / max_measurements, 
-                hops=hops / max_measurements
+                latency=round(latency / measures_count, 8), 
+                hops=round(hops / measures_count, 8),
+                count=measures_count
             )
 
             results.append(measure)
         
-        if count > max_anchors:
+        if anchors_count > max_anchors:
             break
 
     print(f"End of measurements for {country}.")
     return results
 
-
 if __name__ == '__main__':
     anchors = obtain_anchors()
 
-    results = list()
-    max_measurements = 3
+    measures = Measures()
 
     with Pool(4) as pool:
-        results = pool.map(run_measurement_for_country, anchors.items())
-    final_results = [
-        x
-        for xs in results
-        for x in xs
-    ]
+        measures += pool.map(run_measurement_for_country, anchors.items())
 
-    json_str = json.dumps(results, indent=4)
-    with open("measurements.json", "w") as f:
-        f.write(json_str)
+    if measures is not None and len(measures.root) > 0:
+        with open("measurements.json", "w") as f:
+            f.write(measures.model_dump_json()) # type: ignore
 
-    print("Saving measurements... Done!")
+        print("Saving measurements... Done!")
 
