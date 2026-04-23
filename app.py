0ef429210c7402a000e9ed9fa2c953f4ae2d571e
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2026-04-23 18:54:28 +0100

    Yeah socket access doesnt work.

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ import joblib
 from pathlib import Path 
 import pandas as pd
 import streamlit as st
-import socket
-
-from collect import run_measurement
-
-def input_valid(dn_or_ip):
-    try:
-        socket.gethostbyname(dn_or_ip)
-        return True
-    except:
-        return False
 
 dataset = pd.read_csv(
     Path("clean_dataset.csv"), 
@@ -26,24 +16,12 @@ test_set = grouped.get_group("Mullvad")
 ensemble = joblib.load('geolocation_pipeline.pkl')
 
 st.title('Geolocation predicator')
-
-server = st.text_input("IP or domain name", value="wrexham.ac.uk")
-port = st.number_input("Port of the server", min_value=1, max_value=65535, value=53)
-max = st.number_input("Max number of measurements", min_value=1, max_value=3, value=1)
-results = None
+st.subheader("Unfortunately, you have to input the latency and the number of hops manually. Streamlit doesn't allow socket access.")
+latency = st.number_input("Latency in milliseconds", value=13)
+hops = st.number_input("Number of hops", min_value=1, max_value=30, value=10)
 
 if st.button('Run prediction'):
-    if input_valid(server):
-        results = run_measurement(server, port, max)
-    else:
-        st.error("Invalid ip address or domain name.")
+    input_df = pd.DataFrame([[latency, hops]], columns=['latency','hops'])
 
-    if results is not None:
-        latency, hops = results
-
-        input_df = pd.DataFrame([[latency, hops]], columns=['latency','hops'])
-
-        prediction = ensemble.predict(input_df)
-        st.success(f'Predicted geolocation: {prediction}')
-    else:
-        st.error("Couldn't run measurements.")
+    prediction = ensemble.predict(input_df)
+    st.success(f'Predicted geolocation: {prediction}')
