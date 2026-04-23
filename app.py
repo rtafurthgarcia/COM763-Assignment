e15c74ea804de4dfff2a0eb9c980e9fc07451c79
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2026-04-23 18:48:33 +0100

    Add a button

--- app.py
+++ app.py
@@ -31,17 +31,19 @@ server = st.text_input("IP or domain name", value="wrexham.ac.uk")
 port = st.number_input("Port of the server", min_value=1, max_value=65535, value=53)
 max = st.number_input("Max number of measurements", min_value=1, max_value=3, value=1)
 results = None
-if input_valid(server):
-    results = run_measurement(server, port, max)
-else:
-    st.error("Invalid ip address or domain name.")
 
-if results is not None:
-    latency, hops = results
+if st.button('Run prediction'):
+    if input_valid(server):
+        results = run_measurement(server, port, max)
+    else:
+        st.error("Invalid ip address or domain name.")
 
-    input_df = pd.DataFrame([[latency, hops]], columns=['latency','hops'])
+    if results is not None:
+        latency, hops = results
 
-    prediction = ensemble.predict(input_df)
-    st.success(f'Predicted geolocation: {prediction}')
-else:
-    st.error("Couldn't run measurements.")
+        input_df = pd.DataFrame([[latency, hops]], columns=['latency','hops'])
+
+        prediction = ensemble.predict(input_df)
+        st.success(f'Predicted geolocation: {prediction}')
+    else:
+        st.error("Couldn't run measurements.")
