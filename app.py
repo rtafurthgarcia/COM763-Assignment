e76c4f46bdf310b656d5752aad6a48ce45802610
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2026-04-23 18:57:05 +0100

    Just aesthetic changes.

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ test_set = grouped.get_group("Mullvad")
 
 ensemble = joblib.load('geolocation_pipeline.pkl')
 
-st.title('Geolocation predicator')
+st.title('Active measurement-based geolocation')
 st.subheader("Unfortunately, you have to input the latency and the number of hops manually. Streamlit doesn't allow socket access.")
 latency = st.number_input("Latency in milliseconds", value=13)
-hops = st.number_input("Number of hops", min_value=1, max_value=30, value=10)
+hops = st.number_input("Number of hops", min_value=1, max_value=30, value=11)
 
 if st.button('Run prediction'):
     input_df = pd.DataFrame([[latency, hops]], columns=['latency','hops'])
